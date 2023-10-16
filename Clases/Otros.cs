31bffdc470ac7d219dfc6654d6d4ba758eed710f
BarreraJoel <joelmanuelbar@gmail.com>
2023-10-16 20:49:46 -0300

    otro

---
+++ Clases/Otros.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Entidades
+{
+	public class Tarea
+	{
+		/*
+			Creamos la Task y la corremos:
+				Task.Run(Metodo (action));
+
+			Necesitamos: 
+				CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+				CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+			Para cancelar:
+				cancellationTokenSource.Cancel();
+
+			Pregunto si se solicito la cancelacion dentro del metodo que corre la Task:
+				if(!cancellationToken.IsCancellationRequested)
+				{
+					//codigo
+				}
+		*/
+	}
+
+	public class Extensora
+	{
+		/*
+			public static void Extendido(this object obj)
+			{
+				//codigo
+			}
+		*/
+	}
+
+	public class Eventos
+	{
+		/*
+			public delegate void Mostrador(Hora hora, HoraEventArgs e);
+			public event Mostrador HoraMostrada;
+
+			Se puede crear un EventArgs propio.
+			Ejemplo :
+				public class HoraEventArgs : EventArgs
+				{
+					public Hora Hora { get; set };
+
+					public HoraEventArgs(Hora hora)
+					{
+						this.Hora = hora;
+					}
+				}
+
+			Sumar metodo a un evento:
+				textBox.Click += metodo;
+
+			Invocar metodo del evento
+				HoraMostrada.Invoke(datos, new HoraEventArgs(hora));
+		*/
+	}
+}
---
+++ Clases/Serializador.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+//using System.Xml.Serialization;
+//using System.Xml;
+using Newtonsoft.Json;
+
+namespace Entidades
+{
+	public class Serializador
+	{
+		private string path;
+
+		public Serializador()
+		{
+			//this.path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\astros.json";
+		}
+
+		public bool Serializar(List<Astro> lista)
+		{
+			bool serializado = false;
+
+			try
+			{
+				using (StreamWriter sw = new StreamWriter(this.path))
+				{
+					JsonSerializerSettings sett = new JsonSerializerSettings
+					{
+						TypeNameHandling = TypeNameHandling.All
+					};
+					string json = JsonConvert.SerializeObject(lista, Formatting.Indented, sett);
+					sw.Write(json);
+					serializado = true;
+				}
+
+				/*using (StreamWriter sw = new StreamWriter(this.path))
+				{
+					XmlSerializer xml = new XmlSerializer(typeof(List<Astro>));
+					xml.Serialize(sw, lista);
+				}
+				serializado = true;*/
+			}
+			catch (Exception)
+			{
+				serializado = false;
+			}
+
+			return serializado;
+		}
+
+		public List<Astro>? Deserializar()
+		{
+			List<Astro>? lista = new List<Astro>();
+			try
+			{
+				using (StreamReader sr = new StreamReader(this.path))
+				{
+					string json = sr.ReadToEnd();
+					JsonSerializerSettings sett = new JsonSerializerSettings
+					{
+						TypeNameHandling = TypeNameHandling.All
+					};
+					lista = JsonConvert.DeserializeObject<List<Astro>>(json, sett);
+				}
+
+				/*using (StreamReader sr = new StreamReader(this.path))
+				{
+					XmlSerializer xml = new XmlSerializer(typeof(List<Astro>));
+					lista = (List<Astro>)xml.Deserialize(sr);
+				}*/
+			}
+			catch (Exception)
+			{
+				lista = null;
+			}
+
+			return lista;
+		}
+
+	}
+}
